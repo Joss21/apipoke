7c76e1030108a327aa6e71fbf0b88e48878776c8
Joss21 <josselynj98gmail.com>
2022-04-21 15:37:46 -0500

    master

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function navbar() {
+    return (
+        <div className="navbar">
+            Pokemon API
+        </div>
+    );
+}
+
+export default navbar;
